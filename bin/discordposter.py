88f915ab8462ffc0acf8a834fb8745fdd67f097a
wdugan <wesley.dugan@verizonmedia.com>
2020-01-21 13:34:19 -0500

    simplifying

--- bin/discordposter.py
+++ bin/discordposter.py
@@ -1,45 +1,26 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
+'''
+Maintainer: Wesley Dugan
+This is a discord poster
+
+If you would like to use this. You will need to feed it an openweathermap api url
+   a discord/slack url
+'''
 import requests
-import time
-
-POST_URL = "https://discordapp.com/api/webhooks/666187507876233231/7bzJQ66O4T8GeGzip735FgEAacTgdLIqG2FJjZ-4OSjR7cAeMXlu1rpzTdidw1Y9VBmQ"
-CITIES = [5110629, 5907166, 1701667]
-
-class post_it():
-    def timeit(loop_time):
-        start_time = time.time()
-        while time.time() < (start_time + loop_time):
-            time.sleep(loop_time)
-            
-    def bitcoin(post_url):
-        bitcoins = requests.get("https://api.coindesk.com/v1/bpi/currentprice/BTC.json").json()
-        bitcoin_price = bitcoins['bpi']['USD']['rate']
-        payload = {"content": "Bitcoin price: " + bitcoin_price}
-        requests.post(post_url, data=payload)
-
-    def weather(post_url, city):
-        url = "http://api.openweathermap.org/data/2.5/forecast?id={}&APPID=34c57a50fddd3ed0f8a1b9e815acbab8".format(city)
-        weather = requests.get(url).json()
-        weather_temp = weather['list'][0]['main']['temp']
-        weather_humidity = weather['list'][0]['main']['humidity']
-        weather_sky= weather['list'][0]['weather'][0]['description']
-        weather_date = weather['list'][0]['dt_txt']
-        weather_city = weather['city']['name']
-        real_temp = ((weather_temp - 273.15)*1.8)+32
-        payload = {"content": "\nThe forecast for " + weather_date + " is as follows:\
-        	   \n    _Temperature_: " + str(real_temp) + "\n    _Humidity_: " + str(weather_humidity) + "\
-                   \n    _Clearness_: " + weather_sky + "\n    _City_: " + weather_city} 
-        requests.post(post_url, data=payload)  
-
-    def postthis(post_url, cities):
-        try:
-            while True:
-                for the_city in cities:
-                    post_it.weather(post_url, the_city)
-                post_it.bitcoin(post_url)
-                post_it.timeit(10800)
-                
-        except Exception as e:
-            print("Failure!: ", e)
-
-post_it.postthis(POST_URL, CITIES)
+def weather(wurl):
+    '''function to get weather info'''
+    weather_json = requests.get(wurl).json()
+    weather_temp = weather_json['list'][0]['main']['temp']
+    weather_humidity = weather_json['list'][0]['main']['humidity']
+    weather_sky = weather_json['list'][0]['weather'][0]['description']
+    weather_date = weather_json['list'][0]['dt_txt']
+    weather_city = weather_json['city']['name']
+    real_temp = ((weather_temp - 273.15)*1.8)+32
+    payload = {"content": "\nThe forecast for " + weather_date + " is as follows:\
+        \n    _Temperature_: " + str(real_temp) + "\
+        \n    _Humidity_: " + str(weather_humidity) + "\
+        \n    _Clearness_: " + weather_sky + "\n    _City_: " + weather_city}
+    return payload
+def poster(post_url, payload):
+    '''Function to post payload to url'''
+    requests.post(post_url, data=payload)
